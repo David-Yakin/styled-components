4a98403b4aa3764a28885c67b6e6ee8f7e6818ff
David-Yakin <david.yakin@gmail.com>
2024-01-04 10:04:09 +0200

    ButtonStyled

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,5 +1,28 @@
+import React from "react";
+import { ButtonStyled } from "./features/global/styles/components/ButtonStyled";
+import { useTheme } from "./features/global/styles/theme/ThemeProvider";
+
 function App() {
-  return <div>app works!</div>;
+  const { mode, toggleTheme, setMode } = useTheme();
+
+  return (
+    <div style={{ backgroundColor: mode === "dark" ? "black" : "white" }}>
+      <ButtonStyled
+        variant="outline"
+        disabled
+        style={{ marginInlineStart: 12 }}
+      >
+        outline
+      </ButtonStyled>
+      <ButtonStyled variant="outline" style={{ margin: 12 }}>
+        outline
+      </ButtonStyled>
+      <ButtonStyled>plane</ButtonStyled>
+      <ButtonStyled onClick={toggleTheme}>
+        {mode === "light" ? "light" : "dark"}
+      </ButtonStyled>
+    </div>
+  );
 }
 
 export default App;
